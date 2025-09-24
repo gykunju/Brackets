e1f14e2b6d28054e7b1ec2c9baa254038c94b716
gykunju <alvingikunju@gmail.com>
2025-09-24 18:40:21 +0300

    adding pwa functionality

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+})
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.open('brackets-pwa-v1').then((cache) => {
+            return cache.match(event.request).then((response) => {
+                return (
+                    response ||
+                    fetch(event.request).then((networkResponse) => {
+                        if (
+                            event.request.method === "GET" &&
+                            networkResponse && 
+                            networkResponse.status === 200 &&
+                            networkResponse.type === "basic"
+                        ) {
+                            cache.put(event.request, networkResponse.clone())
+                        }
+                        return networkResponse
+                    })
+                )
+            })
+        })
+    )
+})
