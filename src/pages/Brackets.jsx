c3a61fda8466c65bd2605c0058d34e3b568af3ca
gykunju <alvingikunju@gmail.com>
2025-09-28 23:38:51 +0300

    modifications to the navigation and brackets page

--- src/pages/Brackets.jsx
+++ src/pages/Brackets.jsx
@@ -2,24 +2,31 @@ import { IoMdArrowBack } from "react-icons/io";
 import { LuBookMinus } from "react-icons/lu";
 import { Link } from 'react-router'
 import { GrFormNext } from "react-icons/gr";
+import { GrAdd } from "react-icons/gr";
+
 
 function Brackets() {
     const brackets = [
         {
             title: "Semester 1",
             date: new Date("2025-09-27"),
+            active: true,
+            
         },
         {
             title: "Semester 2",
             date: new Date("2025-09-27"),
+            active: true,
         },
         {
             title: "Semester 3",
             date: new Date("2025-09-27"),
+            active: false,
         },
         {
             title: "Semester 4",
             date: new Date("2025-09-27"),
+            active: false,
         },
     ]
 
@@ -32,21 +39,40 @@ function Brackets() {
       <div className="p-5 bg-stone-100">
         <div className="flex justify-center gap-20 items-center relative">
           <h1 className="geist-font wght-700 text-xl">Brackets</h1>
-          <IoMdArrowBack size={28} className="absolute left-0" onClick={() => backPage()}/>
+          <IoMdArrowBack
+            size={28}
+            className="absolute left-0"
+            onClick={() => backPage()}
+          />
+          <GrAdd
+            size={25}
+            className="absolute right-0"
+            onClick={() => backPage()}
+          />
         </div>
       </div>
 
       <div className="flex flex-col gap-5 px-5">
         {brackets.map((bracket) => (
-          <Link to={`/brackets/${bracket.title}`}  className="flex gap-4 items-center p-4 rounded-md shadow-md bg-stone-100 relative">
+          <Link
+            to={`/brackets/${bracket.title}`}
+            className="flex gap-4 items-center p-4 rounded-md shadow-md bg-stone-100 relative"
+          >
             <div className="p-3 border rounded-lg bg-gray-200">
               <LuBookMinus size={28} />
             </div>
-            <div>
-              <h3 className="geist-font wght-800 text-xl">{bracket.title}</h3>
-              <p>{bracket.date.toLocaleDateString()}</p>
+            <div className="w-full flex justify-between items-center">
+              <div>
+                <h3 className="geist-font wght-800 text-xl">{bracket.title}</h3>
+                <p>{bracket.date.toLocaleDateString()}</p>
+              </div>
+              {bracket.active && (
+                <span className="mr-5 p-1 px-3 rounded-3xl text-sm bg-amber-500 ">
+                  current
+                </span>
+              )}
             </div>
-            <GrFormNext className='absolute right-0 text-gray-500' size={24}/>
+            <GrFormNext className="absolute right-0 text-gray-500" size={24} />
           </Link>
         ))}
       </div>
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -2,6 +2,7 @@ import { IoMdArrowBack } from "react-icons/io";
 import { useLocation } from 'react-router'
 import { LuNotebookText } from "react-icons/lu";
 import { GrFormNext } from "react-icons/gr";
+import { GrAdd } from "react-icons/gr";
 
 
 function Courses() {
@@ -13,6 +14,38 @@ function Courses() {
     {
       title: "Systems Programming",
       items: 10,
+    },
+    {
+      title: "Data Analysis",
+      items: 5,
+    },
+    {
+      title: "Statistics",
+      items: 5,
+    },
+    {
+      title: "Calculus 2",
+      items: 8,
+    },
+    {
+      title: "Mathematics for Science",
+      items: 4,
+    },
+    {
+      title: "Data Analysis",
+      items: 5,
+    },
+    {
+      title: "Statistics",
+      items: 5,
+    },
+    {
+      title: "Calculus 2",
+      items: 8,
+    },
+    {
+      title: "Mathematics for Science",
+      items: 4,
     },
     {
       title: "Data Analysis",
@@ -49,18 +82,18 @@ function Courses() {
         </div>
       </div>
 
-      <div className='flex flex-col p-5 gap-5 rounded-lg'>
-        {courses.map(course => (
-            <div className='flex items-center gap-5 relative'>
-                <div className='p-3 rounded-lg bg-stone-800'>
-                    <LuNotebookText size={28} className='text-white'/>
-                </div>
-                <div className=''>
-                    <h3 className='geist-font wght-600 text-lg'>{course.title}</h3>
-                    <p className='text-lime-800'>{course.items} items</p>
-                </div>
-                <GrFormNext className='absolute right-0 text-gray-500' size={24}/>
-            </div> 
+      <div className="flex flex-col p-5 gap-5 rounded-lg">
+        {courses.map((course) => (
+          <div className="flex items-center gap-5 relative">
+            <div className="p-3 rounded-lg bg-stone-800">
+              <LuNotebookText size={28} className="text-white" />
+            </div>
+            <div className="">
+              <h3 className="geist-font wght-600 text-lg">{course.title}</h3>
+              <p className="text-lime-800">{course.items} items</p>
+            </div>
+            <GrFormNext className="absolute right-0 text-gray-500" size={24} />
+          </div>
         ))}
       </div>
     </div>
