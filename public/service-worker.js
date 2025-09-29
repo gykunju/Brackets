4ebdc3c60e960e9e9d052be35a8b1e81853469a7
gykunju <alvingikunju@gmail.com>
2025-09-29 11:33:51 +0300

    changing the versioning on the service-worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,78 @@
+// self.addEventListener("install", (event) => {
+//     self.skipWaiting();
+// })
+
+// self.addEventListener("activate", (event) => {
+//     event.waitUntil(self.clients.claim());
+// });
+
+// self.addEventListener("fetch", (event) => {
+//     event.respondWith(
+//         caches.open('brackets-pwa-v1').then((cache) => {
+//             return cache.match(event.request).then((response) => {
+//                 return (
+//                     response ||
+//                     fetch(event.request).then((networkResponse) => {
+//                         if (
+//                             event.request.method === "GET" &&
+//                             networkResponse && 
+//                             networkResponse.status === 200 &&
+//                             networkResponse.type === "basic"
+//                         ) {
+//                             cache.put(event.request, networkResponse.clone())
+//                         }
+//                         return networkResponse
+//                     })
+//                 )
+//             })
+//         })
+//     )
+// })
+
+
+// 🟣 Change this when you deploy a new version
+const CACHE_NAME = "brackets-pwa-v2";
+
 self.addEventListener("install", (event) => {
-    self.skipWaiting();
-})
+  // Force the new service worker to activate immediately
+  self.skipWaiting();
+});
 
 self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            // Delete old caches when a new version is activated
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.open('brackets-pwa-v1').then((cache) => {
-            return cache.match(event.request).then((response) => {
-                return (
-                    response ||
-                    fetch(event.request).then((networkResponse) => {
-                        if (
-                            event.request.method === "GET" &&
-                            networkResponse && 
-                            networkResponse.status === 200 &&
-                            networkResponse.type === "basic"
-                        ) {
-                            cache.put(event.request, networkResponse.clone())
-                        }
-                        return networkResponse
-                    })
-                )
-            })
-        })
-    )
-})
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse; // Serve from cache
+        }
+        return fetch(event.request).then((networkResponse) => {
+          // Only cache GET requests with successful responses
+          if (
+            event.request.method === "GET" &&
+            networkResponse &&
+            networkResponse.status === 200 &&
+            networkResponse.type === "basic"
+          ) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
