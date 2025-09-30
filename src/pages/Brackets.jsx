628a50e3fd59e0bd45eaf4d007eb6f8277e4f06c
gykunju <alvingikunju@gmail.com>
2025-09-30 23:49:07 +0300

    adding features to the brackets page

--- src/pages/Brackets.jsx
+++ src/pages/Brackets.jsx
@@ -3,7 +3,8 @@ import { LuBookMinus } from "react-icons/lu";
 import { Link } from 'react-router'
 import { GrFormNext } from "react-icons/gr";
 import { GrAdd } from "react-icons/gr";
-
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from 'react'
 
 function Brackets() {
     const brackets = [
@@ -30,12 +31,28 @@ function Brackets() {
         },
     ]
 
+    const [addModal, setAddModal ] = useState(false)
+    const [title, setTitle] = useState()
+
     const backPage = () => {
         window.history.back()
     }
 
+    const addBracket = (e) => {
+        e.preventDefault()
+        let bracket={
+            title: title,
+            date: new Date(),
+            active: true
+        }
+        brackets.push(bracket)
+        console.log(brackets)
+        setTitle("")
+        setAddModal(false)
+    }
+
   return (
-    <div className="geist-font flex flex-col min-h-screen gap-5 bg-[#FCFCF8]">
+    <div className="geist-font flex flex-col min-h-screen gap-5 bg-[#FCFCF8] blur-m">
       <div className="p-5 bg-stone-100">
         <div className="flex justify-center gap-20 items-center relative">
           <h1 className="geist-font wght-700 text-xl">Brackets</h1>
@@ -47,12 +64,12 @@ function Brackets() {
           <GrAdd
             size={25}
             className="absolute right-0"
-            onClick={() => backPage()}
+            onClick={() => setAddModal((prev) => !prev)}
           />
         </div>
       </div>
 
-      <div className="flex flex-col gap-5 px-5">
+      <div className={`flex flex-col gap-5 px-5 ${addModal && "blur-xs"}`}>
         {brackets.map((bracket) => (
           <Link
             to={`/brackets/${bracket.title}`}
@@ -76,6 +93,31 @@ function Brackets() {
           </Link>
         ))}
       </div>
+
+      {addModal && (
+        <div className="flex flex-row items-center justify-center w-fit self-center place-self-center border-3 text-lime-800 absolute z-10 bg-[#FCFCF8] rounded-xl bottom-50 top-10 left-10 right-10 h-45 p-5">
+          <form className="text-center p-3" onSubmit={(e) => addBracket(e)}>
+            <div className="flex relative items-center justify-center mb-4">
+              <h1 className="geist-font wght-700 text-lg">Add Bracket</h1>
+              <AiOutlineClose
+                size={24}
+                className="absolute right-1 border"
+                onClick={() => setAddModal(false)}
+              />
+            </div>
+            <input
+              id="title"
+              placeholder="Add Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border-b-2 p-1 px-2 mb-4 text-x geist-font wght-500"
+            />
+            <button className="geist-font wght-600 border w-full rounded-md p-1 hover:bg-lime-800  hover:text-[#FCFCF8]" type='submit'>
+              Add
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
